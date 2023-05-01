2ebbaa9f50655f938eb213e8c42b709854d6a2f6
ZhengHe-MD <ranchardzheng@gmail.com>
2023-05-01 09:27:36 +0800

    feat: add at_controller to cnat-operator-sdk
    Signed-off-by: ZhengHe-MD <ranchardzheng@gmail.com>

--- cnat-operator-sdk/api/v1alpha1/at_types.go
+++ cnat-operator-sdk/api/v1alpha1/at_types.go
@@ -23,19 +23,30 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+const (
+	PhasePending = "PENDING"
+	PhaseRunning = "RUNNING"
+	PhaseDone    = "DONE"
+)
+
 // AtSpec defines the desired state of At
 type AtSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of At. Edit at_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Schedule...
+	Schedule string `json:"schedule,omitempty"`
+	// Command...
+	Command string `json:"command,omitempty"`
 }
 
 // AtStatus defines the observed state of At
 type AtStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// Phase...
+	Phase string `json:"phase,omitempty"`
 }
 
 //+kubebuilder:object:root=true
--- cnat-operator-sdk/controllers/at_controller.go
+++ cnat-operator-sdk/controllers/at_controller.go
@@ -18,6 +18,15 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -47,10 +56,82 @@ type AtReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
 func (r *AtReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	reqLogger := log.FromContext(ctx).WithValues("namespace", req.Namespace, "at", req.Name)
+	reqLogger.Info("=== Reconciling At")
+	// Fetch the At instance
+	instance := &cnatv1alpha1.At{}
+	err := r.Get(ctx, req.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		reqLogger.Error(err, "error reading the At instance, requeue the request")
+		return ctrl.Result{}, err
+	}
 
-	// TODO(user): your logic here
+	// If no phase set, default to pending (the initial phase):
+	if instance.Status.Phase == "" {
+		instance.Status.Phase = cnatv1alpha1.PhasePending
+	}
 
+	// PENDING -> RUNNING -> DONE
+	switch instance.Status.Phase {
+	case cnatv1alpha1.PhasePending:
+		reqLogger.Info("phase: PENDING")
+		reqLogger.Info("checking schedule", "target", instance.Spec.Schedule)
+		d, err := timeUntilSchedule(instance.Spec.Schedule)
+		if err != nil {
+			reqLogger.Error(err, "schedule parsing failure")
+			// Error reading the schedule. Wait until it is fixed.
+			return ctrl.Result{}, err
+		}
+		reqLogger.Info("schedule parsing done", "result", fmt.Sprintf("diff=%v", d))
+		if d > 0 {
+			return ctrl.Result{RequeueAfter: d}, nil
+		}
+		reqLogger.Info("ready to execute", "command", instance.Spec.Command)
+		instance.Status.Phase = cnatv1alpha1.PhaseRunning
+	case cnatv1alpha1.PhaseRunning:
+		reqLogger.Info("phase: RUNNING")
+		pod := newPodForCR(instance)
+		// Set At instance as the owner and controller
+		if err = ctrl.SetControllerReference(instance, pod, r.Scheme); err != nil {
+			return ctrl.Result{}, err
+		}
+		found := &corev1.Pod{}
+		err = r.Get(ctx, client.ObjectKeyFromObject(pod), found)
+		if err != nil && errors.IsNotFound(err) {
+			err = r.Create(ctx, pod)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+			reqLogger.Info("pod launched", "name", pod.Name)
+		} else if err != nil {
+			return ctrl.Result{}, err
+		} else if found.Status.Phase == corev1.PodFailed || found.Status.Phase == corev1.PodSucceeded {
+			reqLogger.Info("container terminated",
+				"reason", found.Status.Reason,
+				"message", found.Status.Message)
+			instance.Status.Phase = cnatv1alpha1.PhaseDone
+		} else {
+			// pod is pending, running or in an unknown state, don't requeue
+			// in this case because we'll be called again when the pod status changes.
+			return ctrl.Result{}, nil
+		}
+	case cnatv1alpha1.PhaseDone:
+		reqLogger.Info("phase: DONE")
+		return ctrl.Result{}, nil
+	default:
+		reqLogger.Info("NOP")
+		return ctrl.Result{}, nil
+	}
+
+	// Update the At instance, setting the status to the respective phase
+	if err = r.Status().Update(ctx, instance); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// Don't requeue - we will reconcile again when there's a change to the object.
 	return ctrl.Result{}, nil
 }
 
@@ -58,5 +139,38 @@ func (r *AtReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Re
 func (r *AtReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&cnatv1alpha1.At{}).
+		Watches(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
+			OwnerType:    &cnatv1alpha1.At{},
+			IsController: true,
+		}).
 		Complete(r)
+}
+
+func newPodForCR(cr *cnatv1alpha1.At) *corev1.Pod {
+	labels := map[string]string{"app": cr.Name}
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cr.Name + "-pod",
+			Namespace: cr.Namespace,
+			Labels:    labels,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:    "busybox",
+					Image:   "busybox",
+					Command: strings.Split(cr.Spec.Command, " "),
+				},
+			},
+			RestartPolicy: corev1.RestartPolicyOnFailure,
+		},
+	}
+}
+
+func timeUntilSchedule(schedule string) (time.Duration, error) {
+	s, err := time.Parse("2006-01-02T15:04:05Z", schedule)
+	if err != nil {
+		return time.Duration(0), err
+	}
+	return s.Sub(time.Now()), nil
 }
